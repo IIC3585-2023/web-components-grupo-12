6b09080bb4c1b616c7bc09e1b4ce0630eddb83b5
Camila González <ccgonzalez3@uc.cl>
2023-06-19 15:04:48 -0400

    Add style and functions

--- web-component/tree-item.js
+++ web-component/tree-item.js
@@ -2,15 +2,21 @@ const template = document.createElement('template');
 
 template.innerHTML = `
   <style>
-    .tree-item {
-      margin-left: 15px;
-      display: none;
-    }
-    .tree-item.visible {
-      display: block;
+    :host {
+      font-family: Arial, sans-serif;
     }
     .toggle {
       cursor: pointer;
+      padding: 5px;
+      background-color: #DFE9FF;
+      margin-bottom: 5px;
+      border-radius: 5px;
+    }
+    .toggle:hover {
+      background-color: #BFD3FF;
+    }
+    .tree-item {
+      margin-left: 30px;
     }
   </style>
   <div class="toggle"></div>
@@ -19,11 +25,10 @@ template.innerHTML = `
   </div>
 `;
 
-
 class TreeItem extends HTMLElement {
   constructor() {
     super();
-    this.expanded = false;
+    this.isExpanded = false;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.$toggle = this.shadowRoot.querySelector('.toggle');
@@ -32,27 +37,44 @@ class TreeItem extends HTMLElement {
 
   connectedCallback() {
     setTimeout(() => {
-      let childNodes = Array.from(this.childNodes);
-      let textNode = childNodes.find(node => node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== '');
-      if (textNode) {
-          this.label = textNode.nodeValue.trim();
-          this.removeChild(textNode);
-      } else {
-        this.label = '';
-      }
+      this.label = this.getLabel();
       this.$toggle.addEventListener('click', () => this.toggle());
       this.render();
     });
   }
 
+  getLabel() {
+    let childNodes = Array.from(this.childNodes);
+    let textNode = childNodes.find(node => node.nodeType === Node.TEXT_NODE && node.nodeValue !== '');
+    this.removeChild(textNode);
+    return textNode.nodeValue.trim();
+  }
+
   toggle() {
-    this.expanded = !this.expanded;
+    this.isExpanded = !this.isExpanded;
     this.render();
   }
 
   render() {
-    this.$toggle.textContent = (this.expanded ? '▼ ' : '► ') + this.label;
-    this.$children.style.display = this.expanded ? 'block' : 'none';
+    this.$toggle.textContent = this.getIcon() + this.label;
+    this.$children.style.display = this.isExpanded ? 'block' : 'none';
+  }
+
+  getIcon() {
+    if (this.hasChildren()) {
+      if (this.isExpanded) {
+        return '▼ ';
+      } else {
+        return '► ';
+      }
+    } else {
+      return ' ';
+    }
+  }
+
+  hasChildren() {
+    let childNodes = Array.from(this.childNodes);
+    return childNodes.some(node => node.nodeType != Node.TEXT_NODE);
   }
 }
 
